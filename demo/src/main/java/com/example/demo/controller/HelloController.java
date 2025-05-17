2031f59f17a604e71c8eb778a1b89c89cd45887f
arapovad <maleha-7@internet.ru>
2025-05-17 11:34:15 +0400

    Done w/o logging

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,29 +1,24 @@
 package com.example.demo.controller;
 
-//import com.example.demo.model.Message;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.example.demo.model.Message;
+
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+//@Slf4j
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
+//        log.info("Пользователь отправил GET запрос на /hello");
         return "Привет, мир!";
     }
 
     @PostMapping("/echo")
     public Message echo(@RequestBody Message input) {
+//        log.info("Пользователь отправил POST запрос на /echo");
         return input;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    static class Message {
-        private String text;
     }
 }
