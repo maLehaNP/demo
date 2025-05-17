0a3601bded301edd2bf1a2c17b03f92234da2f92
arapovad <maleha-7@internet.ru>
2025-05-17 12:10:54 +0400

    Fully done with logging

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,23 +2,23 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Message;
 
-//import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-//@Slf4j
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-//        log.info("Пользователь отправил GET запрос на /hello");
+        log.info("Пользователь отправил GET запрос на /hello");
         return "Привет, мир!";
     }
 
     @PostMapping("/echo")
     public Message echo(@RequestBody Message input) {
-//        log.info("Пользователь отправил POST запрос на /echo");
+        log.info("Пользователь отправил POST запрос на /echo");
         return input;
     }
 }
